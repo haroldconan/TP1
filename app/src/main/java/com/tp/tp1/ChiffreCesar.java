8ce4e3eebe2f5e317050d5a8b1869ae260a48400
haroldconan <28harold04@gmail.com>
2019-11-05 10:20:53 +0100

    hc : init2

---
+++ app/src/main/java/com/tp/tp1/ChiffreCesar.java
@@ -0,0 +1,43 @@
+/**
+ * Created by lanoix-a on 06/11/2017.
+ */
+
+public class ChiffreCesar {
+
+    static char[] ALPHABET = {' ',
+                              'A','B','C','D','E',
+                              'F','G','H','I','J',
+                              'K','L','M','N','O',
+                              'P','Q','R','S','T',
+                              'U','V','W','X','Y','Z'};
+
+    public static String chiffrement(String chaine, int decalage) {
+
+        char[] chInitial = chaine.toUpperCase().toCharArray();
+        char[] chResultat = new char[chInitial.length];
+
+        for (int i = 0; i < chInitial.length; i++) {
+            int posInitiale = position(chInitial[i]);
+            if (posInitiale == -1) {
+                chResultat[i] = chInitial[i];
+            }
+            else {
+                int npos = (posInitiale + decalage) % 26;
+                if (npos == 0) {
+                    npos = 26;
+                }
+                chResultat[i] = ALPHABET[npos];
+            }
+        }
+        return new String(chResultat);
+    }
+
+    private static int position(char c)  {
+        for(int i = 1; i < ALPHABET.length; i++)   {
+            if(ALPHABET[i] == c) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
---
+++ app/src/test/java/com/tp/tp1/ChiffreCesarTest.java
@@ -0,0 +1,63 @@
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ChiffreCesarTest {
+
+    @Test
+    public void chiffreCesar_decalage0() throws Exception {
+        assertEquals("ABCDEF", ChiffreCesar.chiffrement("ABCDEF",0));
+    }
+
+    @Test
+    public void chiffreCesar_decalage1() throws Exception {
+        assertEquals("BCDEFG", ChiffreCesar.chiffrement("ABCDEF",1));
+    }
+
+    @Test
+    public void chiffreCesar_minuscules_decalage1() throws Exception {
+        assertEquals("BCDEFG", ChiffreCesar.chiffrement("abcdef",1));
+    }
+
+    @Test
+    public void chiffreCesar_minuscules_decalage4() throws Exception {
+        assertEquals("EFGHIJ", ChiffreCesar.chiffrement("abcdef",4));
+    }
+
+    @Test
+    public void chiffreCesar_caracteresNonChiffres() throws Exception {
+        assertEquals("EFG &;HIJ", ChiffreCesar.chiffrement("abc &;def",4));
+    }
+
+    @Test
+    public void chiffreCesar_Z_decalage1() throws Exception {
+        assertEquals("A", ChiffreCesar.chiffrement("Z",1));
+    }
+
+    @Test
+    public void chiffreCesar_Z_decalage3() throws Exception {
+        assertEquals("C", ChiffreCesar.chiffrement("Z",3));
+    }
+
+    @Test
+    public void chiffreCesar_decalage26() throws Exception {
+        assertEquals("ABCDEF", ChiffreCesar.chiffrement("ABCDEF",26));
+    }
+
+    @Test
+    public void chiffreCesar_double_decalage13() throws Exception {
+        assertEquals("ABCDEF", ChiffreCesar.chiffrement(ChiffreCesar.chiffrement("ABCDEF",13),13));
+    }
+
+    @Test
+    public void chiffreCesar_Y_decalage1() throws Exception {
+        assertEquals("Z", ChiffreCesar.chiffrement("Y",1));
+    }
+
+    @Test
+    public void chiffreCesar_X_decalage2() throws Exception {
+        assertEquals("Z", ChiffreCesar.chiffrement("X",2));
+    }
+
+}
