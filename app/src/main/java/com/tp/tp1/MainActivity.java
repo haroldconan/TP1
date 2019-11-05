4f5ea1960bdc3ca125cd15a9b4d2f3ce0d99203a
haroldconan <28harold04@gmail.com>
2019-11-05 10:20:18 +0100

    hc : init2

--- app/src/main/java/com/tp/tp1/MainActivity.java
+++ app/src/main/java/com/tp/tp1/MainActivity.java
@@ -3,12 +3,22 @@ package com.tp.tp1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private static final String TAG = "MESSAGE_HC";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(getApplicationContext(),
+                " Hello ␣ World ␣ !!!! ",
+                Toast.LENGTH_SHORT).show();
+        Log.i(TAG, " Hello ␣ World ␣ !!!! ");
+
     }
 }
